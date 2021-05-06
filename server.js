4028dc3d87c36a81909ba96260ebe8d2bf78f8b2
KerimAksak <abdulkerim.aksak@outlook.com>
2021-05-06 23:18:13 +0300

    Server listen

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const app = require("./app");
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
-server.listen(port, hostname, () => {
+server.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
